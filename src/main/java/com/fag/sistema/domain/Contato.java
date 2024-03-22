82ec9b5631576422080939e25fec3c92e9816f67
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-03-21 21:42:50 -0300

    feat: create class contato

---
+++ src/main/java/com/fag/sistema/domain/Contato.java
@@ -0,0 +1,13 @@
+package com.fag.sistema.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contato {
+  private String telefone;
+  private String email;
+}
