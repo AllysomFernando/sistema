aadbbe83a6db90ef43abe2705f767ba1dc8b404b
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-03-21 21:29:45 -0300

    adding class dependente

---
+++ src/main/java/com/fag/sistema/domain/Dependente.java
@@ -0,0 +1,13 @@
+package com.fag.sistema.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Dependente {
+    public String nome;
+    public Date data_nascimento;
+}
