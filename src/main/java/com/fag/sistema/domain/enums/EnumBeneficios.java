db449bdea0f61ed6ca759c3ccd5a4730132ed52a
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-03-26 11:10:39 -0300

    implementing the math to EnumBeneficios

--- src/main/java/com/fag/sistema/domain/enums/EnumBeneficios.java
+++ src/main/java/com/fag/sistema/domain/enums/EnumBeneficios.java
@@ -1,14 +1,71 @@
 package com.fag.sistema.domain.enums;
 
+import java.math.BigDecimal;
+
 public enum EnumBeneficios {
-  HORA_EXTRA,
-  COMISSAO,
-  SALARIO_MATERNIDADE,
-  QUINQUENIO,
-  ADICIONAL_NOTURNO,
-  ADICIONAL_INSALUBRIDADE,
-  ADICIONAL_PERICULOSIDADE,
-  SALARIO_FAMILIA,
-  AUXILIO_CRECHE,
-  DIARIAS_VIAGEM;
+  HORA_EXTRA {
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      BigDecimal valorHoraExtra = salarioBruto.divide(new BigDecimal(220), 2, BigDecimal.ROUND_HALF_UP);
+      return valorHoraExtra.multiply(new BigDecimal("0.5"));
+    }
+  },
+  COMISSAO {
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      BigDecimal valorVendas = BigDecimal.valueOf(1000);
+      return valorVendas.multiply(new BigDecimal("0.06"));
+    }
+  },
+  SALARIO_MATERNIDADE{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  QUINQUENIO{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  ADICIONAL_NOTURNO{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  ADICIONAL_INSALUBRIDADE{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  ADICIONAL_PERICULOSIDADE{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  SALARIO_FAMILIA{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  AUXILIO_CRECHE{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  },
+  DIARIAS_VIAGEM{
+    @Override
+    public BigDecimal calculate(BigDecimal salarioBruto) {
+      return null;
+    }
+  };
+
+  public abstract BigDecimal calculate(BigDecimal salarioBruto);
+
 }
