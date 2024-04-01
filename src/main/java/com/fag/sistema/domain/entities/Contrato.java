887b15dfc02ac379a592f258fe1efe959da25d87
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-01 01:33:51 -0300

    refactor: implementing enum to adicional insalubridade

--- src/main/java/com/fag/sistema/domain/entities/Contrato.java
+++ src/main/java/com/fag/sistema/domain/entities/Contrato.java
@@ -2,6 +2,7 @@ package com.fag.sistema.domain.entities;
 
 import java.sql.Date;
 
+import com.fag.sistema.domain.enums.EnumGrauInsalubridade;
 import com.fag.sistema.domain.enums.EnumHorarioTrabalho;
 
 import lombok.AllArgsConstructor;
@@ -17,7 +18,7 @@ public class Contrato {
     private String funcao;
     private int pisPasep;
     private int horas;
-    private Integer grauInsalubridade;
+    private EnumGrauInsalubridade grauInsalubridade = EnumGrauInsalubridade.NULO;
     private Empregador empregador;
     private Salario salario;
     private EnumHorarioTrabalho horarioTrabalho = EnumHorarioTrabalho.COMERCIAL;
--- src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalInsalubridade.java
+++ src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalInsalubridade.java
@@ -3,23 +3,24 @@ package com.fag.sistema.domain.usecases.calcular.beneficios;
 import java.math.BigDecimal;
 
 import com.fag.sistema.domain.entities.Empregado;
+import com.fag.sistema.domain.enums.EnumGrauInsalubridade;
 
 public class AdicionalInsalubridade implements IBeneficioUseCase {
 
   @Override
   public BigDecimal calculate(Empregado empregado) {
-    Integer grauInsalubridade = empregado.getContrato().getGrauInsalubridade();
+    EnumGrauInsalubridade grauInsalubridade = empregado.getContrato().getGrauInsalubridade();
     BigDecimal salarioBruto = empregado.getContrato().getSalario().getBruto();
 
-    if (grauInsalubridade == 10) {
+    if (grauInsalubridade == EnumGrauInsalubridade.BAIXO) {
       return salarioBruto.multiply(new BigDecimal("0.1"));
     }
 
-    if (grauInsalubridade == 20) {
+    if (grauInsalubridade == EnumGrauInsalubridade.MEDIO) {
       return salarioBruto.multiply(new BigDecimal("0.2"));
     }
 
-    if (grauInsalubridade == 40) {
+    if (grauInsalubridade == EnumGrauInsalubridade.ALTO) {
       return salarioBruto.multiply(new BigDecimal("0.4"));
     }
     
