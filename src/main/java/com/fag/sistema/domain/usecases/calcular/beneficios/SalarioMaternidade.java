3df97c8d4d766c88f9069f7ade1982c9db7ecf4e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-01 04:10:19 -0300

    test: salario maternidade tests

--- src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/SalarioMaternidade.java
+++ src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/SalarioMaternidade.java
@@ -1,15 +1,17 @@
 package com.fag.sistema.domain.usecases.calcular.beneficios;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import com.fag.sistema.domain.entities.Empregado;
 
-public class SalarioMaternidade implements IBeneficioUseCase{
+public class SalarioMaternidade implements IBeneficioUseCase {
 
   @Override
   public BigDecimal calculate(Empregado empregado) {
-    BigDecimal beneficiolMensal = empregado.getContrato().getSalario().getBruto().multiply(new BigDecimal("0.05"));
-      return beneficiolMensal.multiply(new BigDecimal("6"));
+    BigDecimal salarioBruto = empregado.getContrato().getSalario().getBruto();
+    BigDecimal beneficiolMensal = salarioBruto.multiply(new BigDecimal("0.05"));
+    return beneficiolMensal.setScale(2, RoundingMode.DOWN);
   }
-  
+
 }
---
+++ src/test/java/com/fag/sistema/domain/usecases/calcular/beneficios/SalarioMaternidadeTest.java
@@ -0,0 +1,31 @@
+package com.fag.sistema.domain.usecases.calcular.beneficios;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+import com.fag.sistema.domain.entities.Contrato;
+import com.fag.sistema.domain.entities.Empregado;
+import com.fag.sistema.domain.entities.Salario;
+
+public class SalarioMaternidadeTest {
+  private Empregado makeEmpregado(BigDecimal salarioBruto) {
+    Empregado empregado = new Empregado();
+    empregado.setContrato(new Contrato());
+    empregado.getContrato().setSalario(new Salario(salarioBruto));
+
+    return empregado;
+  }
+
+  @Test
+  void shouldCalculateSalarioMaternidade() {
+    SalarioMaternidade sut = new SalarioMaternidade();
+    Empregado empregado = makeEmpregado(new BigDecimal("2000.00"));
+    
+    BigDecimal beneficio = sut.calculate(empregado);
+    
+    assertEquals(new BigDecimal("100.00"), beneficio);
+  }
+}
