48b7d5f5661f01fe7900e026ec33eb3b22912a5f
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-02 09:31:41 -0300

    respondendo o TODO

--- src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalNoturno.java
+++ src/main/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalNoturno.java
@@ -8,17 +8,22 @@ import com.fag.sistema.domain.enums.EnumHorarioTrabalho;
 
 public class AdicionalNoturno implements IBeneficioUseCase {
 
-  // TODO: Baseado no valor da hora ou no salario bruto?
-  @Override
-  public BigDecimal calculate(Empregado empregado) {
-    EnumHorarioTrabalho horarioTrabalho = empregado.getContrato().getHorarioTrabalho();
+    // TODO: Baseado no valor da hora trabalhda
+    @Override
+    public BigDecimal calculate(Empregado empregado) {
+        EnumHorarioTrabalho horarioTrabalho = empregado.getContrato().getHorarioTrabalho();
 
-    if (horarioTrabalho == EnumHorarioTrabalho.NOTURNO) {
-      BigDecimal salarioBruto = empregado.getContrato().getSalario().getBruto();
-      return salarioBruto.multiply(new BigDecimal("0.15")).setScale(2, RoundingMode.DOWN);
+        if (horarioTrabalho == EnumHorarioTrabalho.NOTURNO) {
+            BigDecimal salarioBruto = empregado.getContrato().getSalario().getBruto();
+            int totalHoraMensais = empregado.getContrato().getHoras();
+
+
+            BigDecimal valorHora = salarioBruto.divide(new BigDecimal(totalHoraMensais), RoundingMode.HALF_UP);
+            int horasNoturnas = 0;//TODO: pegar as horas trabalhadas no periodo noturno;
+            return salarioBruto.multiply(new BigDecimal("0.15")).setScale(2, RoundingMode.DOWN);
+        }
+
+        return BigDecimal.ZERO;
     }
-
-    return BigDecimal.ZERO;
-  }
 
 }
