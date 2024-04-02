63949f392fbfb6e0e981f6a0a1e743a6089d38ef
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-02 11:20:24 -0300

    adding and implementging the JsonFileReader and EmpregadoRepository class

--- src/main/java/com/fag/sistema/infrastructure/adapters/json/JsonFileReader.java
+++ src/main/java/com/fag/sistema/infrastructure/adapters/json/JsonFileReader.java
@@ -1,22 +1,24 @@
 package com.fag.sistema.infrastructure.adapters.json;
 
 import com.fag.sistema.domain.entities.Empregado;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class JsonFileReader {
-    public static void main(String[] args){
-        ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper;
+
+    public JsonFileReader() {
+        objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+    }
 
-        try{
-            Empregado empregado = objectMapper.readValue(new File("src/java/com/fag/sistema/assets/dados.json"), Empregado.class);
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public List<Empregado> readEmpregadoList(String jsonFilePath) throws IOException {
+        return objectMapper.readValue(new File(jsonFilePath), new TypeReference<List<Empregado>>() {
+        });
     }
 }
--- src/main/java/com/fag/sistema/infrastructure/repository/EmpregadoRepository.java
+++ src/main/java/com/fag/sistema/infrastructure/repository/EmpregadoRepository.java
@@ -1,2 +1,31 @@
-package com.fag.sistema.infrastructure.repository;public class EmpregadoRepository {
+package com.fag.sistema.infrastructure.repository;
+
+import com.fag.sistema.domain.entities.Empregado;
+import com.fag.sistema.domain.repositories.IEmpregadoVendor;
+import com.fag.sistema.infrastructure.adapters.json.JsonFileReader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EmpregadoRepository implements IEmpregadoVendor {
+    private Map<String, Empregado> empregados = new HashMap<>();
+
+    public EmpregadoRepository(String jsonFilePath){
+        JsonFileReader jsonFileReader = new JsonFileReader();
+        try{
+            List<Empregado> empregadosList = jsonFileReader.readEmpregadoList(jsonFilePath);
+            for (Empregado empregado : empregadosList){
+                this.empregados.put(empregado.getCpf(), empregado);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public List<Empregado> getAllEmpregados() {
+        return new ArrayList<>(empregados.values());
+    }
 }
