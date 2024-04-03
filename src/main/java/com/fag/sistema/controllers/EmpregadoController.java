6f32b2c8dd9e2fa47ef37a6cedd03f7e8a73eb06
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-03 14:51:28 -0300

    adding routes

---
+++ src/main/java/com/fag/sistema/controllers/EmpregadoController.java
@@ -0,0 +1,12 @@
+package com.fag.sistema.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/empregado")
+public class EmpregadoController {
+
+}
+
