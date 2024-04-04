4659cb684d155f3170c56b709e46993ce58902ef
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-04 16:03:10 -0300

    Implementing route to getEmpregadoByCPF

--- src/main/java/com/fag/sistema/controllers/EmpregadoController.java
+++ src/main/java/com/fag/sistema/controllers/EmpregadoController.java
@@ -3,7 +3,9 @@ package com.fag.sistema.controllers;
 
 import com.fag.sistema.domain.dto.EmpregadoDTO;
 import com.fag.sistema.service.EmpregadoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,17 @@ public class EmpregadoController {
         this.empregadoService = empregadoService;
     }
 
-    @GetMapping("/listar")
+    @GetMapping
     public List<EmpregadoDTO> listarEmpregados(){
         return empregadoService.getAllEmpregados();
+    }
+    @GetMapping("/{cpf}")
+    public ResponseEntity<EmpregadoDTO> listarEmpregadoPorCPF(@PathVariable String cpf){
+        EmpregadoDTO empregadoDTO = empregadoService.getEmpregadoporCPF(cpf);
+        if(empregadoDTO!=null){
+            return ResponseEntity.ok(empregadoDTO);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
