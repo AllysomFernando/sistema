71f382c7102ce926d4550f70aa8a8d83c55445a4
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-04 10:47:39 -0300

    removing disnecessary atributes from Dependente class

--- src/main/java/com/fag/sistema/domain/entities/Dependente.java
+++ src/main/java/com/fag/sistema/domain/entities/Dependente.java
@@ -10,11 +10,5 @@ import lombok.*;
 public class Dependente {
     private String nome;
     private LocalDate dataNascimento;
-    private String parentesco;
-    private String genero;
 
-    public Dependente (String nome, LocalDate dataNascimento) {
-        this.nome = nome;
-        this.dataNascimento = dataNascimento;
-    }
 }
