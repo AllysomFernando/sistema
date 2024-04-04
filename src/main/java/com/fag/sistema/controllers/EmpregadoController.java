d6e55eb8e30fe84e0be563d70bda805483bbd6e1
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-04 15:10:22 -0300

    Implementing the EmpregadoController and defining the routes

--- src/main/java/com/fag/sistema/controllers/EmpregadoController.java
+++ src/main/java/com/fag/sistema/controllers/EmpregadoController.java
@@ -1,11 +1,25 @@
 package com.fag.sistema.controllers;
 
 
+import com.fag.sistema.domain.dto.EmpregadoDTO;
+import com.fag.sistema.service.EmpregadoService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/empregado")
+@RequestMapping("/empregados")
 public class EmpregadoController {
 
+    private EmpregadoService empregadoService;
+    public EmpregadoController(EmpregadoService empregadoService) {
+        this.empregadoService = empregadoService;
+    }
+
+    @GetMapping("/listar")
+    public List<EmpregadoDTO> listarEmpregados(){
+        return empregadoService.getAllEmpregados();
+    }
 }
---
+++ src/main/java/com/fag/sistema/service/EmpregadoService.java
@@ -0,0 +1,2 @@
+package com.fag.sistema.service;public class EmpregadoService {
+}
