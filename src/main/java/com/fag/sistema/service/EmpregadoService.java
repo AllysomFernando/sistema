77fd79b913e81e2bd5d12f73aea8c1d34d4088f2
AllysomFernando <allysom.renczenczen18@gmail.com>
2024-04-04 15:12:44 -0300

    Adding and implementing the EmpregadoService class

--- src/main/java/com/fag/sistema/service/EmpregadoService.java
+++ src/main/java/com/fag/sistema/service/EmpregadoService.java
@@ -1,2 +1,27 @@
-package com.fag.sistema.service;public class EmpregadoService {
+package com.fag.sistema.service;
+
+
+import com.fag.sistema.domain.dto.EmpregadoDTO;
+import com.fag.sistema.domain.entities.Empregado;
+import com.fag.sistema.domain.mappers.EmpregadoMapper;
+import com.fag.sistema.infrastructure.repositories.EmpregadoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EmpregadoService {
+    private EmpregadoRepository empregadoRepository;
+
+    public EmpregadoService(EmpregadoRepository empregadoRepository) {
+        this.empregadoRepository = empregadoRepository;
+    }
+
+    public List<EmpregadoDTO> getAllEmpregados() {
+        List<Empregado> empregadosList = empregadoRepository.getAllEmpregados();
+        return empregadosList.stream()
+                .map(EmpregadoMapper::toDTO)
+                .collect(Collectors.toList());
+    }
 }
