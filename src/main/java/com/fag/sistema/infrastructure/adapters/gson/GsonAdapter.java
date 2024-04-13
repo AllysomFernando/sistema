0dfe53d2ecd244aedc1522621a318d34e4119573
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-13 17:18:22 -0300

    test: ensure gson adapter reads file, returns content as list and as array

--- src/main/java/com/fag/sistema/infrastructure/adapters/gson/GsonAdapter.java
+++ src/main/java/com/fag/sistema/infrastructure/adapters/gson/GsonAdapter.java
@@ -1,31 +1,69 @@
 package com.fag.sistema.infrastructure.adapters.gson;
 
+import java.io.FileReader;
 import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.nio.file.FileSystems;
 import java.util.Collections;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import com.fag.sistema.domain.entities.Empregador;
 import com.fag.sistema.infrastructure.interfaces.IJsonReader;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 @Component
-public class GsonAdapter implements IJsonReader {
+public class GsonAdapter<T> implements IJsonReader<T> {
 
   private Gson gson;
+  private Type listType = new TypeToken<List<T>>() {
+  }.getType();
 
-  public GsonAdapter() {
+  private String fileName;
+
+  public GsonAdapter(String fileName) {
     this.gson = new Gson();
+    this.fileName = fileName;
   }
+
   @Override
-  public List<Empregador> read() {
+  public T read() {
     InputStream resource = this.getClass().getClassLoader().getResourceAsStream("empregado.json");
     if (resource == null) {
       throw new RuntimeException("Arquivo não encontrado no diretório");
     }
-    List<Empregador> data = Collections.singletonList(gson.fromJson(resource.toString(), Empregador.class));
+
+    List<T> data = Collections.singletonList(gson.fromJson(resource.toString(), listType));
+    return null;
+  }
+
+  @Override
+  public List<T> readListFromJson() {
+    JsonReader reader = new JsonReader(this.getResource());
+
+    List<T> data = gson.fromJson(reader, this.listType);
     return data;
+  }
+
+  @Override
+  public T[] readArrayFromJson() {
+    JsonReader reader = new JsonReader(this.getResource());
+
+    T[] data = gson.fromJson(reader, this.listType);
+    return data;
+  }
+
+  public FileReader getResource() {
+    String filePath = FileSystems.getDefault().getPath("src/main/resources/" + this.fileName).toAbsolutePath()
+        .toString();
+    try {
+      FileReader file = new FileReader(filePath);
+      return file;
+    } catch (Exception e) {
+      throw new RuntimeException("Arquivo não encontrado");
+    }
   }
 
 }
---
+++ src/test/java/com/fag/sistema/infrastructure/adapters/gson/GsonAdapterTest.java
@@ -0,0 +1,46 @@
+package com.fag.sistema.infrastructure.adapters.gson;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.fag.sistema.domain.entities.Empregador;
+
+public class GsonAdapterTest {
+
+  @Test
+  void shouldNotThrowIfValidResourceIsGiven() {
+    GsonAdapter<Object> sut = new GsonAdapter<Object>("data.json");
+
+    assertNotNull(sut.getResource());
+    assertDoesNotThrow(() -> {
+      sut.getResource();
+    });
+  }
+
+  @Test
+  void shouldThrowIfInvalidResourceIsGiven() {
+    GsonAdapter<Object> sut = new GsonAdapter<Object>("invalid-file.json");
+
+    assertThrows(RuntimeException.class, () -> {
+      sut.getResource();
+    });
+  }
+
+  @Test
+  void shouldReadJsonContentAsList() {
+    GsonAdapter<Empregador> sut = new GsonAdapter<Empregador>("data.json");
+
+    List<Empregador> empregadores = sut.readListFromJson();
+
+    assertNotNull(empregadores);
+    assertEquals(1, empregadores.size());
+
+  }
+
+}
