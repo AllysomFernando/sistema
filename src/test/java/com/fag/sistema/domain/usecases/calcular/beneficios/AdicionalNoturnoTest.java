dbd3facdad6e0b6ce1dce0951dd58679b82d99db
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-13 20:53:46 -0300

    fix: test class

--- src/test/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalNoturnoTest.java
+++ src/test/java/com/fag/sistema/domain/usecases/calcular/beneficios/AdicionalNoturnoTest.java
@@ -9,15 +9,24 @@ import org.springframework.context.annotation.Description;
 
 import com.fag.sistema.domain.entities.Contrato;
 import com.fag.sistema.domain.entities.Empregado;
+import com.fag.sistema.domain.entities.Horario;
 import com.fag.sistema.domain.entities.Salario;
-import com.fag.sistema.domain.enums.EnumHorarioTrabalho;
 
 public class AdicionalNoturnoTest {
-  private Empregado makeEmpregado(BigDecimal salarioBruto, EnumHorarioTrabalho horarioTrabalho) {
+  private Empregado makeEmpregado(BigDecimal salarioBruto, Float horasAdicionalNoturno) {
     Empregado empregado = new Empregado();
-    empregado.setContrato(new Contrato());
-    empregado.getContrato().setSalario(new Salario(salarioBruto));
-    empregado.getContrato().setHorarioTrabalho(horarioTrabalho);
+    Contrato contrato = new Contrato();
+
+    Salario salario = new Salario();
+    salario.setBase(salarioBruto);
+
+    Horario horario = new Horario();
+    horario.setHorasAdicionalNoturno(horasAdicionalNoturno);
+    horario.setHoraTrabalhada(220);
+
+    empregado.setContrato(contrato);
+    contrato.setSalario(salario);
+    empregado.setHorario(horario);
 
     return empregado;
   }
@@ -26,18 +35,18 @@ public class AdicionalNoturnoTest {
   @Description("Should add adicional noturno if horario de trabalho is noturno")
   void shouldAddAdicionaNoturno() {
     AdicionalNoturno sut = new AdicionalNoturno();
-    Empregado empregado = makeEmpregado(new BigDecimal("1900.0"), EnumHorarioTrabalho.NOTURNO);
+    Empregado empregado = makeEmpregado(new BigDecimal("1900.0"), 10.0f);
 
     BigDecimal beneficio = sut.calculate(empregado);
 
-    assertEquals(new BigDecimal("285.00"), beneficio);
+    assertEquals(new BigDecimal("12.90"), beneficio);
   }
 
   @Test
   @Description("Should not add adicional noturno if horario de trabalho is not noturno")
   void shouldNotAddAdicionaNoturno() {
     AdicionalNoturno sut = new AdicionalNoturno();
-    Empregado empregado = makeEmpregado(new BigDecimal("1900.0"), EnumHorarioTrabalho.COMERCIAL);
+    Empregado empregado = makeEmpregado(new BigDecimal("1900.0"), 0.0f);
 
     BigDecimal beneficio = sut.calculate(empregado);
 
