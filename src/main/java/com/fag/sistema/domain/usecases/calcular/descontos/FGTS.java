9e5288e2f2d05b979dd004673c216aa56b03c230
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-04-14 00:36:31 -0300

    refactor: fgts extends provento

--- src/main/java/com/fag/sistema/domain/usecases/calcular/descontos/FGTS.java
+++ src/main/java/com/fag/sistema/domain/usecases/calcular/descontos/FGTS.java
@@ -1,18 +1,28 @@
 package com.fag.sistema.domain.usecases.calcular.descontos;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.springframework.stereotype.Service;
 
+import com.fag.sistema.domain.entities.Provento;
 import com.fag.sistema.domain.entities.empregado.Empregado;
 
 @Service
-public class FGTS implements IDescontoUseCase {
+public class FGTS extends Provento {
 
-  @Override
+  public FGTS() {
+    this.setDescricao("FGTS");
+  }
+
   public BigDecimal calculate(Empregado empregado) {
     BigDecimal salarioBruto = empregado.getContrato().getSalario().getBruto();
-    return salarioBruto.multiply(new BigDecimal("0.08"));
+    BigDecimal referencia = new BigDecimal("0.08");
+    BigDecimal desconto = salarioBruto.multiply(referencia).setScale(2, RoundingMode.DOWN);
+
+    this.setProvento(getDescricao(), referencia, BigDecimal.ZERO, desconto);
+
+    return desconto;
   }
 
 }
